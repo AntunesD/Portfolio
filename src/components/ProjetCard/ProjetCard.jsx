5f9bbecef1feabe2eaf17ff11954ce585ef6696d
AntunesD <antunesd90@gmail.com>
2025-03-20 15:10:42 +0100

    CardEffect

--- src/components/ProjetCard/ProjetCard.jsx
+++ src/components/ProjetCard/ProjetCard.jsx
@@ -1,4 +1,7 @@
 import './ProjetCard.scss';
+import './CardEffect.scss'
+
+import React, { useRef, useState } from "react";
 
 import { useDispatch } from 'react-redux';
 import { openModal } from '../../redux/reducer/modalSlice';
@@ -22,34 +25,112 @@ function ProjetCard({ title, place, imgTitle, outil, gif, description, cara, git
   };
 
 
+
+  /*pour cardEffect*/
+
+  const cardRef = useRef(null);
+  const transitionRef = useRef(null);
+  const [styles, setStyles] = useState({
+    x: "",
+    y: "",
+    bgX: "",
+    bgY: "",
+    rX: "",
+    rY: "",
+  });
+
+  const handleMouseMove = (e) => {
+    const el = cardRef.current;
+    const transition = transitionRef.current;
+
+    transition.style.transition = "none";
+
+    let w = el.clientWidth;
+    let h = el.clientHeight;
+    let b = el.getBoundingClientRect();
+
+    let X = (e.clientX - b.left) / w;
+    let Y = (e.clientY - b.top) / h;
+
+    let rX = -(X - 0.5) * 26;
+    let rY = (Y - 0.5) * 26;
+
+    let bgX = 40 + 20 * X;
+    let bgY = 40 + 20 * Y;
+
+    console.log(X, Y);
+
+    setStyles({
+      x: 100 * X + "%",
+      y: 100 * Y + "%",
+      bgX: bgX + "%",
+      bgY: bgY + "%",
+      rX: rX + "deg",
+      rY: rY + "deg",
+    });
+  };
+
+  const handleMouseOut = () => {
+    const transition = transitionRef.current;
+
+    transition.style.transition = "all 0.5s";
+    setStyles({
+      x: "",
+      y: "",
+      bgX: "",
+      bgY: "",
+      rX: "",
+      rY: "",
+    });
+  };
+
+
   return (
     <div className={`slider ${place}`} onClick={Click} >
-      <div className={`bordercard `} >
-        <div className={`${Background} projetcard `}>
-          <div className="card-header">
-            <h3>
-              <img src={imgTitle} alt='Titre du projet' />
-            </h3>
-            <Bulle logo={logo}/>
-          </div>
-          <div className="card-image">
-            <video autoPlay loop muted>
-              <source src={gif} type="video/webm"/>
-                Votre navigateur ne prend pas en charge la vidéo au format WebM.
-            </video>
 
-            <p>{cara}</p>
-          </div>
-          <div className="card-details">
-            <h4>Description : </h4>
-            <p>{description}</p>
-            <p className='enSavoir' onClick={openModalHandler} >En Savoir <i className="fa-regular fa-circle-xmark"></i></p>
+      <div ref={cardRef} className="cardEffect" onMouseMove={handleMouseMove}
+        onMouseOut={handleMouseOut}>
+        <div className="cardEffect__wrapper">
+          <div ref={transitionRef} className="cardEffect__3d" style={{
+            "--r-x": styles.rX,
+            "--r-y": styles.rY
+          }}>
+
+
+            <div className={`bordercard `} >
+              <div className={`${Background} projetcard `}>
+                <div className="card-header">
+                  <h3>
+                    <img src={imgTitle} alt='Titre du projet' />
+                  </h3>
+                  <Bulle logo={logo} />
+                </div>
+                <div className="card-image">
+                  <video autoPlay loop muted>
+                    <source src={gif} type="video/webm" />
+                    Votre navigateur ne prend pas en charge la vidéo au format WebM.
+                  </video>
+
+                  <p>{cara}</p>
+                </div>
+                <div className="card-details">
+                  <h4>Description : </h4>
+                  <p>{description}</p>
+                  <p className='enSavoir' onClick={openModalHandler} >En Savoir <i className="fa-regular fa-circle-xmark"></i></p>
+                </div>
+              </div>
+              <p className='fincard' >©2023 Antunes J. David/Portfolio</p>
+            </div>
+
+            <div className="cardEffect__layer1" style={{
+              "--x": styles.x,
+              "--y": styles.y
+            }} ></div>
+
           </div>
         </div>
-        <p className='fincard' >©2023 Antunes J. David/Portfolio</p>
       </div>
     </div>
-
   );
 }
 
--- src/containers/Accueil/Accueil.jsx
+++ src/containers/Accueil/Accueil.jsx
@@ -3,12 +3,12 @@ import "./Accueil.scss";
 function Accueil() {
     return (
         <section id="Accueil">
-                <h2>Développeur Intégrateur Web JavaScript et React</h2>
+            <h2>Développeur Full Stack</h2>
             <div>
                 <p>Bienvenue sur le portfolio de <b>David J. Antunes</b>.</p>
-                    <p>À 33 ans, développeur intégrateur web, spécialisé en JavaScript et React, il embrasse avec enthousiasme le monde de la programmation. Malgré son statut de débutant, il est déterminé à acquérir les compétences nécessaires pour exceller dans son domaine.
-                    </p>
-                    <p>Parcourez son profil en découvrant ses projets et contactez-le pour avoir un nouveau collaborateur compétent et ambitieux parmi vous.</p>
+                <p>À 35 ans, développeur full stack, spécialisé en TypeScript et React, il embrasse avec enthousiasme le monde de la programmation. Malgré son statut de débutant, il est déterminé à acquérir les compétences nécessaires pour exceller dans son domaine.
+                </p>
+                <p>Parcourez son profil en découvrant ses projets et contactez-le pour avoir un nouveau collaborateur compétent et ambitieux parmi vous.</p>
             </div>
         </section>
     )
